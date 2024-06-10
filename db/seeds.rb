c931780e7d6733a71c5ea60ca6682b5642204570
Biliarto Sastro Cemerson <139372393+billycemerson@users.noreply.github.com>
2024-06-10 08:33:19 +0000

    Simulasi Data Artikel

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,12 @@
 
 data = {full_name: 'Deny Siregar', email: 'denysiregar@gmail.com', password: '1234abcd'}
 user = User.create(data)
+
+user = User.first
+articles = [
+    {title: 'Title article 1', description: 'Description article 1', user: user},
+    {title: 'Title article 2', description: 'Description article 2', user: user},
+    {title: 'Title article 3', description: 'Description article 3', user: user}
+]
+
+user = Article.create(articles)

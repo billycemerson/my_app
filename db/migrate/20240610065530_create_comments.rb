ce936b2306c288139568ed8ba4d299ce264f84a6
Biliarto Sastro Cemerson <139372393+billycemerson@users.noreply.github.com>
2024-06-10 07:17:22 +0000

    Membuat Database dan Controller

---
+++ db/migrate/20240610065530_create_comments.rb
@@ -0,0 +1,10 @@
+class CreateComments < ActiveRecord::Migration[5.2]
+  def change
+    create_table :comments do |t|
+      t.string :belongs_to
+      t.string :comment
+
+      t.datetime
+    end
+  end
+end

e71a11add31e43be83b0da44acd704d3ac9b2e53
Biliarto Sastro Cemerson <139372393+billycemerson@users.noreply.github.com>
2024-06-12 07:26:49 +0000

    Memperbaiki Error Kirim Komentar

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_06_10_065530) do
+ActiveRecord::Schema.define(version: 2024_06_12_072509) do
 
   create_table "articles", force: :cascade do |t|
     t.integer "user_id"
@@ -22,8 +22,9 @@ ActiveRecord::Schema.define(version: 2024_06_10_065530) do
   end
 
   create_table "comments", force: :cascade do |t|
-    t.string "belongs_to"
     t.string "comment"
+    t.integer "article_id"
+    t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
   create_table "users", force: :cascade do |t|
